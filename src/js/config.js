254ff9cd58ad8cc9951fae6aec34111c81ce1727
Panagiotis Tsiavos <panos20.pt@gmail.com>
2021-09-29 16:33:51 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a96f581e-5c3a-4f28-9714-669af5679302';
+export const MODAL_CLOSE_SEC = 2.5;
